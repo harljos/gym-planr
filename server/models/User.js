734fc135a4ec213c6316b3caed47555ca787c19f
unknown <joseph.harley02@gmail.com>
2023-04-06 14:22:20 -0400

    completed user model

---
+++ server/models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+// schema for user model
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function (v) {
+                return /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    excercises: [
+        {
+            type: String
+        }
+    ]
+});
+
+// hashes password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+});
+
+// validates password login
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = model("User", userSchema);
+
+module.exports = User;
