851a4260ac216aea1fa13872b6d5f4bd60a7cb22
unknown <joseph.harley02@gmail.com>
2023-05-26 23:02:11 -0400

    changed styling for sign up page

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,14 +1,14 @@
 const SignUp = () => {
   return (
-    <div className="bg-black h-screen flex items-center justify-center">
-      <div className="bg-orange-500 rounded-lg p-8">
+    <div className="bg-orange-700 h-screen flex items-center justify-center">
+      <div className="bg-orange-700 rounded-lg p-8">
         <h1 className="text-2xl font-bold mb-4 text-white">Sign Up</h1>
         <form>
           <div className="mb-4">
             <label className="text-white block mb-2">Email</label>
             <input
               type="email"
-              className="bg-orange-200 px-4 py-2 rounded w-full"
+              className="bg-orange-500 px-4 py-2 rounded w-full"
               placeholder="Enter your email"
             />
           </div>
@@ -16,11 +16,11 @@ const SignUp = () => {
             <label className="text-white block mb-2">Password</label>
             <input
               type="password"
-              className="bg-orange-200 px-4 py-2 rounded w-full"
+              className="bg-orange-500 px-4 py-2 rounded w-full"
               placeholder="Enter your password"
             />
           </div>
-          <button className="bg-orange-700 text-white px-4 py-2 rounded w-full">
+          <button className="bg-orange-500 text-white px-4 py-2 rounded w-full">
             Sign Up
           </button>
         </form>
