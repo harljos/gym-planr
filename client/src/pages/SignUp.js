e7a96fa37f27ef9e8ee6b0c0ea81261e653a1a4b
unknown <joseph.harley02@gmail.com>
2023-07-03 12:54:58 -0400

    added handle change function that changes the user form data state when a field is changed

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,4 +1,18 @@
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutations";
+
 const SignUp = () => {
+  const [userFormData, setUserFormData] = useState({ username: " ", email: " ", password: " " });
+  const [addUser, { error, data }] = useMutation(ADD_USER);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+
+
+  }
+
   return (
     <div className="bg-orange-700 h-screen flex items-center justify-center">
       <div className="bg-orange-700 rounded-lg p-8">
